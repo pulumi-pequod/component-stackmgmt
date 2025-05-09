8c465a4940031cc73c3dff26dc1e4d3c4aee91cf
MitchellGerdisch <mitch@pulumi.com>
2025-05-08 20:28:30 -0500

    tweaks

--- stackSettings.ts
+++ stackSettings.ts
@@ -46,34 +46,34 @@ export class StackSettings extends pulumi.ComponentResource {
       if (deploymentConfig != null) { // it's not a no-code deployment
         // Set the stack's deployment settings based on what was returned by the buildDeploymentSettings function.
         const deploymentSettings = new pulumiservice.DeploymentSettings(`${name}-deployment-settings`, deploymentConfig, {parent: this, retainOnDelete: true})
-        //// TTL Schedule ////
-        // Calculate the TTL time based on the TTL minutes passed in or default to 8 hours.
-        const ttlTime = new pulumitime.Offset("ttltime", {offsetMinutes: (args.ttlMinutes || (8*60))}, { parent: this }).rfc3339
-        const ttlSchedule = new pulumiservice.TtlSchedule(`${name}-ttlschedule`, {
-          organization: org,
-          project: project,
-          stack: stack,
-          timestamp: ttlTime,
-          deleteAfterDestroy: false,
-        }, { parent: this, dependsOn: [deploymentSettings] }) 
-
-        //// Drift Schedule ////
-        let remediation = true // assume we want to remediate
-        if ((args.driftManagement) && (args.driftManagement != "Correct")) {
-          remediation = false // only do drift detection
-        }
-        const driftSchedule = new pulumiservice.DriftSchedule(`${name}-driftschedule`, {
-          organization: org,
-          project: project,
-          stack: stack,
-          scheduleCron: "0 * * * *",
-          autoRemediate: remediation,
-        }, { parent: this, dependsOn: [deploymentSettings] }) 
       } else {
         // Need to set the delete_stack tag to "StackOnly" to prevent the purge automation from trying to delete the repo which points at the 
         // templates repo - we definitely don't want to delete the templates repo.
         setTag(stackFqdn, deleteStackTagName, "StackOnly")
       }
+      //// TTL Schedule ////
+      // Calculate the TTL time based on the TTL minutes passed in or default to 8 hours.
+      const ttlTime = new pulumitime.Offset("ttltime", {offsetMinutes: (args.ttlMinutes || (8*60))}, { parent: this }).rfc3339
+      const ttlSchedule = new pulumiservice.TtlSchedule(`${name}-ttlschedule`, {
+        organization: org,
+        project: project,
+        stack: stack,
+        timestamp: ttlTime,
+        deleteAfterDestroy: false,
+      }, { parent: this }) 
+
+      //// Drift Schedule ////
+      let remediation = true // assume we want to remediate
+      if ((args.driftManagement) && (args.driftManagement != "Correct")) {
+        remediation = false // only do drift detection
+      }
+      const driftSchedule = new pulumiservice.DriftSchedule(`${name}-driftschedule`, {
+        organization: org,
+        project: project,
+        stack: stack,
+        scheduleCron: "0 * * * *",
+        autoRemediate: remediation,
+      }, { parent: this }) 
     })
 
     //// Team Stack Assignment ////
